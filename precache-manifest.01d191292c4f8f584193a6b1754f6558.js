b5b8efc052623c6e3f55a1bcaca3c03a4be3b644
shazzad <szzd@users.noreply.github.com>
2020-04-29 00:11:58 +0600

    first commit

---
+++ precache-manifest.01d191292c4f8f584193a6b1754f6558.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e339ec9546595c7b29c3ec6f6985a1e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "820295e2bd1952635c47",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "1fc5d8f6a5a11dc3d30a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "820295e2bd1952635c47",
+    "url": "/static/js/2.65880925.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65880925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc5d8f6a5a11dc3d30a",
+    "url": "/static/js/main.da3c1d03.chunk.js"
+  },
+  {
+    "revision": "354fca099e28b2b9d27e",
+    "url": "/static/js/runtime-main.5f7695d3.js"
+  }
+]);
